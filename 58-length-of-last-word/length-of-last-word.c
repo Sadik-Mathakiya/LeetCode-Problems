d97a0c338b0195d638b756bb6e9b73dca63afac0
Sadik Mathakiya <sadikmathakiya07@gmail.com>
2026-07-11 20:34:49 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (79.84%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.c
+++ 58-length-of-last-word/length-of-last-word.c
@@ -2,13 +2,9 @@ int lengthOfLastWord(char* s) {
     int len = strlen(s);
     int c = 0;
     int i = len - 1;
-
-    // Step 1: Skip all trailing spaces (safely!)
-    while (i >= 0 && s[i] == ' ') {
+        while (i >= 0 && s[i] == ' ') {
         i--;
     }
-
-    // Step 2: Count the length of the last word (safely!)
     while (i >= 0 && s[i] != ' ') {
         c++;
         i--;
